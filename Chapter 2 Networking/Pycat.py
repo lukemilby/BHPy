77745ecb356f3f29e80f629dc733007e89a58e8f
Thetides <luke.milby@gmail.com>
2014-12-16 14:38:58 -0600

    adding more to Pycat

--- Chapter 2 Networking/Pycat.py
+++ Chapter 2 Networking/Pycat.py
@@ -49,4 +49,75 @@ def main():
 		usage()
 
 	for o,a in opts:
-		if o in ("-h","--help")
+		if o in ("-h","--help"):
+			usage()
+		elif o in ("-l","--listen"):
+			listen = True
+		elif o in ("-e","--execute"):
+			execute = a
+		elif o in ("c", "--commandshell"):
+			command = True
+		elif o in ("-u","--upload"):
+			upload_destination = a
+		elif o in ("-t","--target"):
+			target = a
+		elif o in ("-p","--port"):
+			port = int(a)
+		else:
+			assert False,"Unhandled Option"
+	# are we going to listen or just send data from stdin?
+	if no listen and len(target) and port >0:
+		#read in the buffer from the commandline
+		#this will block, so send CTRL-D if not sending input
+		#to stdin
+		buffer = sys.stdin.read()
+
+		#send data off
+		client_sender(buffer)
+
+
+	#we are going to listen and potentially
+	#upload things, execute commands, and drop a shell back
+	#depending on our command line options above
+	if listen:
+		server_loop()
+
+main()
+
+def client_sender(buffer):
+
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	try:
+		#connect to our target host
+		client.connect((target,port))
+
+		if len(buffer):
+			client.send(buffer)
+
+			while True:
+				#now wait for data back
+				recv_len = 1
+				response = ""
+
+				while recv_len:
+
+					data = client.recv(4096)
+					recv_len = len(data)
+					response+= data
+
+					if recv_len < 4096:
+						break
+			print response,
+
+			#wait for more input
+			buffer = raw_input("")
+			buffer += "\n"
+
+			#send it off
+			client.send(buffer)
+	
+	except:
+
+		print "[*] Exception! Exiting"
+		raise e
